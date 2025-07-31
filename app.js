33ecb447f7a093d65dd83517c85ed32b7e9100c1
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-31 06:00:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,32 @@ function saludar() {
 function saludarConNombre(nombre) {
     return console.log(`¡Hola ${nombre}!`);
 }
+// desafío 3
+function dobledenumero(numero) {
+    numero = parseInt(prompt("Ingresa un número:"));
+    return console.log(`El doble de ${numero} es ${numero * 2}`);
+}
+// desafío 4
+function promedio(a,b,c){
+    a = parseFloat(prompt("Ingresa el primer número:"));
+    b = parseFloat(prompt("Ingresa el segundo número:"));
+    c = parseFloat(prompt("Ingresa el tercer número:"));
+    return console.log(`El promedio de ${a}, ${b} y ${c} es ${(a + b + c) / 3}`);
+}
+// desafío 5
+function mayorOmenor(a,b){
+    a = parseFloat(prompt("Ingresa el primer número:"));
+    b = parseFloat(prompt("Ingresa el segundo número:"));
+    if (a > b) {
+        return console.log(`${a} es mayor que ${b}`);
+    } else if (a < b) {
+        return console.log(`${a} es menor que ${b}`);
+    } else {
+        return console.log(`${a} es igual a ${b}`);
+    }
+}
+// desafío 6
+function multiplicarxunomismo(numero) {
+    numero = parseInt(prompt("Ingresa un número:"));
+    return console.log(`El resultado de multiplicar ${numero} por si mismo es ${numero * numero}`);
+}
