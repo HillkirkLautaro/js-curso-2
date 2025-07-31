8890cebbcb82585fc92316855e4a58b70c29f8f2
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-31 00:37:52 -0300

    Update
    primeros dos desafios

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+// desafío 1
+document.querySelector("h1").innerHTML = "Hora del desafío";
+// desafío 2
+function consoleclick() {
+    console.log("¡Hiciste clic en el botón!");
+}
+// desafío 3
 
