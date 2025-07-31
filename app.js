9c896ec2655184b43500e5f5944d50a82fced678
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-31 05:56:26 -0300

    Udpate

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 // desafío 1
-document.querySelector("h1").innerHTML = "Hora del desafío";
+function saludar() {
+    return console.log(`¡Hola mundo!`);
+}
 // desafío 2
-function consoleclick() {
-    console.log("¡Hiciste clic en el botón!");
-}
-// desafío 3
-function BrasilQuestion() {
-    let ciudad = prompt("¿Qué ciudad de Brasil te gusta?");
-    alert(`Estuve en ${ciudad} y me acordé de ti.`);
-}
-// desafío 4
-function promptclick() {
-    alert("Yo amo JS")
-}
-// desafío 5
-function suma() {
-    var num1 = parseInt(prompt("Ingrese el primer número:"));
-    var num2 = parseInt(prompt("Ingrese el segundo número:"));
-    var resultado = num1 + num2;
-    alert(`El resultado de la suma es: ${resultado}`);
-}
