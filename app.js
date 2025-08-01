59d53ca4fbbb385bde5f3d33310cf07638f3e58e
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-08-01 08:37:04 -0300

    Update

--- app.js
+++ app.js
@@ -1,38 +1,10 @@
-// desafío 1
-function saludar() {
-    return console.log(`¡Hola mundo!`);
+// práctica 1
+function calculoIMC(peso, altura) {
+    alert("El indice de masa corporal (IMC) se calcula como el peso en kg dividido por la altura en metros al cuadrado.");
+    peso = parseFloat(prompt("Ingresa tu peso en kg:"));
+    altura = parseFloat(prompt("Ingresa tu altura en metros:"));
+    const imc = peso / (altura * altura);
+    return console.log(`Tu IMC es ${imc}`);
 }
-// desafío 2
-function saludarConNombre(nombre) {
-    nombre = prompt("Ingresa tu nombre:");
-    return console.log(`¡Hola ${nombre}!`);
-}
-// desafío 3
-function dobledenumero(numero) {
-    numero = parseInt(prompt("Ingresa un número:"));
-    return console.log(`El doble de ${numero} es ${numero * 2}`);
-}
-// desafío 4
-function promedio(a,b,c){
-    a = parseFloat(prompt("Ingresa el primer número:"));
-    b = parseFloat(prompt("Ingresa el segundo número:"));
-    c = parseFloat(prompt("Ingresa el tercer número:"));
-    return console.log(`El promedio de ${a}, ${b} y ${c} es ${(a + b + c) / 3}`);
-}
-// desafío 5
-function mayorOmenor(a,b){
-    a = parseFloat(prompt("Ingresa el primer número:"));
-    b = parseFloat(prompt("Ingresa el segundo número:"));
-    if (a > b) {
-        return console.log(`${a} es mayor que ${b}`);
-    } else if (a < b) {
-        return console.log(`${a} es menor que ${b}`);
-    } else {
-        return console.log(`${a} es igual a ${b}`);
-    }
-}
-// desafío 6
-function multiplicarxunomismo(numero) {
-    numero = parseInt(prompt("Ingresa un número:"));
-    return console.log(`El resultado de multiplicar ${numero} por si mismo es ${numero * numero}`);
-}
+
+// práctica 2
